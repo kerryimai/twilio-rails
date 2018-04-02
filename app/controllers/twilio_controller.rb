b8958d348e5c4bdf3b49a381bab2c60937b37cac
Kerry Choy <kchoy@xogrp.com>
2018-04-02 19:04:16 -0400

    complete option 2 for voicemail

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,27 +3,33 @@ class TwilioController < ApplicationController
   def voice
     response = Twilio::TwiML::VoiceResponse.new
     response.gather(action: '/twilio/number', method: 'GET') do |gather|
-      gather.say('Please enter your account number,\nfollowed by the pound sign')
+      gather.say('Hi there, press 1 if you would like to speak with Kerry, or press 2 if you would like to leave a voicemail followed by the pound sign')
     end
     response.say('We didn\'t receive any input. Goodbye!')
     render :xml => response.to_xml
   end
 
-  def gather
-    response = Twilio::TwiML::VoiceResponse.new
-    response.gather(input: 'speech dtmf', timeout: 3, num_digits: 1) do |gather|
-      gather.say('Please press 1 or say sales for sales.')
-    end
-  end
 
   def number
-    if params['Digits']
-      response = Twilio::TwiML::VoiceResponse.new
+    p params['Digits'] == '2'
+    response = Twilio::TwiML::VoiceResponse.new
+    if params['Digits'] == '1'
       response.dial do |dial|
           dial.number('408-891-7592')
       end
+    elsif params['Digits'] == '2'
+      response.say('Please leave a message at the beep.\nPress the star key when finished.')
+      response.record(action: '/twilio/voicemail', method: 'GET',
+        max_length: 20, finish_on_key: '*')
+      response.say('I did not receive a recording')
     end
     render :xml => response.to_xml
   end
+
+def voicemail
+  byebug
+
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'twilio/voice' => 'twilio#voice'
   get 'twilio/number' => 'twilio#number'
+  get 'twilio/voicemail' => 'twilio#voicemail'
 
 end
